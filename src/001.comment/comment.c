1abe9c7c3c87771991b996e021635987859b391e
김민석 <minseok7654@gmail.com>
2021-04-24 07:45:46 +0000

    주석실습

---
+++ src/001.comment/comment.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	/*
+	가
+	나
+	다
+	*/
+	
+	//한줄 주석 goorm에서 ctrl누르고 /누르면 한번에 주석처리
+	// printf("Hi, Computer \n");
+	// printf("Hi, Human \n");
+	printf("Hello, World \n");	   
+	return 0;
+}
