c248f5e09f668ec48fcdd99882c4ec24c8e7685a
김민석 <minseok7654@gmail.com>
2021-04-24 07:24:36 +0000

    Hellowordc첫프로그램

---
+++ src/000.Hello.World/helloworld.c
@@ -0,0 +1,7 @@
+#include <stdio.h> //# = 전처리기 (컴파일 전에 미리 처리) ,stdio = standard input output
+
+int main() //프로그램의 시작, int = 정수형 변수
+{
+	printf("Hello Word \n"); //printf = 괄호안의 내용을 출력, 문자열은 "로 묶음, 1행의 stdio.h 파일을 include하여 사용 가능
+	return 0; //0을 반환한다는 의미 main 함수를 호출한 프로그램(운영체제)에게 반환, 정상적으로 종료한다는 의미
+}
