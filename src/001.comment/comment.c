bc1317988342b611d577109bcc38948714545126
김민석 <minseok7654@gmail.com>
2021-04-24 08:53:38 +0000

    주석

--- src/001.comment/comment.c
+++ src/001.comment/comment.c
@@ -9,8 +9,8 @@ int main()
 	*/
 	
 	//한줄 주석 goorm에서 ctrl누르고 /누르면 한번에 주석처리
-	// printf("Hi, Computer \n");
-	// printf("Hi, Human \n");
+	printf("Hi, Computer \n");
+	printf("Hi, Human \n");
 	printf("Hello, World \n");	   
 	return 0;
 }
