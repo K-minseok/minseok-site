351eff1fac225600bd1c9d4b047522d9d8e10124
김민석 <minseok7654@gmail.com>
2021-04-25 07:39:50 +0000

    여러가지변수형모음

--- src/003.InputOutputfunction/ScanExercise.c
+++ src/003.InputOutputfunction/ScanExercise.c
@@ -2,35 +2,35 @@
 
 int main()
 {
-	char ch;
+    char ch;
+    
+    short sh;
+    int i;
+    long lo;
+    
+    float fl;
+    double du;
+    
+    printf("char 형 변수 입력 : ");
+    scanf("%c", &ch);
+    
+    printf("short 형 변수 입력 : ");
+    scanf("%hd", &sh);
 	
-	short sh;
-	int i;
-	long lo;
+    printf("int 형 변수 입력 : ");
+    scanf("%d", &i);
 	
-	float fl;
-	double du;
+    printf("long 형 변수 입력 : ");
+    scanf("%ld", &lo);
+    
+    printf("float 형 변수 입력 : ");
+    scanf("%f", &fl);
 	
-	printf("char형 변수 입력 : ");
-	scanf("%c", &ch);
-	
-	printf("short형 변수 입력 : ");
-	scanf("%hd", &sh);
-	
-	printf("int형 변수 입력 : ");
-	scanf("%d", &i);
-	
-	printf("long형 변수 입력 : ");
-	scanf("%ld", &lo);
-	
-	printf("float형 변수 입력 : ");
-	scanf("%f", &fl);
-	
-	printf("double형 변수 입력 : ");
-	scanf("%lf", &du);
-	
-	printf("char : %c, short : %d, int : %d \n", ch, sh, i);
-	printf("long : %ld, float : %f, double : %f \n", lo, fl, du);
-	
-	return 0;
+    printf("double 형 변수 입력 : ");
+    scanf("%lf", &du);
+    
+    printf("char : %c, short : %d, int : %d \n", ch, sh, i);
+    printf("long : %ld, float : %f, double : %f \n", lo, fl, du);
+    
+    return 0;
 }
