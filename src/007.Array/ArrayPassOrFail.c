c30799a13e58260af1d0229c79c932a457400c86
김민석 <minseok7654@gmail.com>
2021-05-02 08:19:57 +0000

    평균 계산 후 결과 출력

--- src/007.Array/ArrayPassOrFail.c
+++ src/007.Array/ArrayPassOrFail.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 int main()
 {
-	int arr[10];
-	int i, ave = 0;
+	double arr[10];
+	int i;
+	double ave = 0;
 	for(i=0; i<10; i++)
 	{
 		printf("%d번째 학생의 성적은? ", i+1);
-		scanf("%d", &arr[i]);
+		scanf("%lf", &arr[i]); //double과 lf와 같이 구문이 맞게 맞춰주기
 	}
 	for(i=0; i<10; i++)
 	{
 		ave = ave + arr[i];
 	}
 	ave = ave / 10;
-	printf("전체 학생의 평균은 : %d \n", ave);
+	printf("전체 학생의 평균은 : %.2f \n", ave);
 	for(i=0; i<10; i++)
 	{
 		printf("학생 %d : ", i + 1);
