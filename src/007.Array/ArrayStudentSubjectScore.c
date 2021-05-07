2b870cde4e7faa93b58028f20fc49cf5f68f38a2
김민석 <minseok7654@gmail.com>
2021-05-07 23:39:54 +0000

    평균추가

--- src/007.Array/ArrayStudentSubjectScore.c
+++ src/007.Array/ArrayStudentSubjectScore.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 int main()
 {
-	int score[3][2];
+	int score[3][3];
 	int i, j;
+	float k;
 	
 	for(i=0; i<3; i++)
 	{
-		for(j=0; j<2; j++)
+		for(j=0; j<3; j++)
 		{
 			if(j==0)
 			{
@@ -18,12 +19,17 @@ int main()
 				printf("%d번째 학생의 수학 점수 : ", i+1);
 				scanf("%d", &score[i][j]);
 			}
+			else
+			{
+				printf("%d번째 학생의 영어 점수 : ", i+1);
+				scanf("%d", &score[i][j]);
+			}
 		}
 	}
-	
 	for(i=0; i<3; i++)
 	{
-		printf("%d번째 학생의 국어 점수 : %d, 수학 점수 : %d \n", i+1, score[i][0], score[i][1]);
+		k = score[i][0] + score[i][1] + score[i][2];
+		printf("%d번째 학생의 국어 점수 : %d, 수학 점수 : %d, 영어 점수 : %d, 세 과목의 평균 : %.2f \n", i+1, score[i][0], score[i][1], score[i][2], k / 3);
 	}
 	
 	return 0;
