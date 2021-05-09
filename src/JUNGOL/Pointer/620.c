42eab2a77ead8a74a467217a53f1fb8fd4ecbdb4
김민석 <minseok7654@gmail.com>
2021-05-09 08:47:38 +0000

    jungol

--- src/JUNGOL/Pointer/620.c
+++ src/JUNGOL/Pointer/620.c
@@ -2,10 +2,11 @@
 int main(){
 	int *p;
 	int i;
-	p = &i;
-	
+		
 	printf("정수를 입력하세요 : ");
-	scanf("%d", &*p);
+	scanf("%d", &i);
+	
+	p = &i;
 	
 	printf("%d...%d \n", *p/10, *p%10);
 	printf("%d...%d \n", i/10, i%10);
